9b8f620db0a57444388ef17ccb800a0edd08db74
Paulo Salvatore <paulo@salvatore.tech>
2020-09-22 16:54:30 -0300

    Preparação para rodar no Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
